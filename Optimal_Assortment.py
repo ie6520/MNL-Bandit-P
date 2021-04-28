8f41297b708de5ecd2d529b41af01227eee99c4b
FireworksZ <zhaohuangjie518@gmail.com>
2021-04-28 22:34:25 +0800

    update optimal assortment solver, write pao-ts framework

--- Optimal_Assortment.py
+++ Optimal_Assortment.py
@@ -1,6 +1,7 @@
 import cplex
+import numpy as np
 
-def getOptimalAssortment(n,v,r,C,log = True):
+def getOptimalAssortment(n,w,r,B,log = True):
     
     p = cplex.Cplex()
     if not log:
@@ -11,23 +12,26 @@ def getOptimalAssortment(n,v,r,C,log = True):
         
     obj = [0]+r
     p.objective.set_sense(p.objective.sense.maximize)
-    p.variables.add(obj = obj,names = ['w'+str(i) for i in range(n+1)])
+    p.variables.add(obj = obj,names = ['x'+str(i) for i in range(n+1)])
     rows = []
     rowP = [1 for i in range(n+1)]
-    rowC = [-C]+[1.0/v[i] for i in range(n)]
+    rowC = [-B]+[1.0/v[i] for i in range(n)]
     va = [i for i in range(n+1)]
     rows = [[va,rowP],[va,rowC]]
     for i in range(1,n+1):
         temp = [0 for j in range(n+1)]
-        temp[i] = 1/v[i-1] 
+        temp[i] = 1/w[i-1] 
         temp[0] = -1
         rows.append([va,temp])
     rhs = [0 for i in range(n+2)]
     rhs[0] = 1
-    p.linear_constraints.add(lin_expr=rows,senses="L"*(n+2),rhs = rhs)
-    p.write("lpex.lp")
+    senses = ['L']*(n+2)
+    senses[0] = 'E'
+    p.linear_constraints.add(lin_expr=rows,senses=senses,rhs = rhs)
+    #p.write("lpex.lp")
+    
     p.solve()
-    val = p.solution.get_objective_value()
+    #val = p.solution.get_objective_value()
     re = p.solution.get_values()
     
     #print(re)
@@ -35,16 +39,15 @@ def getOptimalAssortment(n,v,r,C,log = True):
     
     x = []
     for i in range(1,n+1):
-        if re[i]>0: x.append(i)
+        if re[i]>0: x.append(i-1)
     return x
     
 
 if __name__=='__main__':
     n = 5
-    v = [1 for i in range(1,n+1)]
-    r = [i for i in range(1,n+1)]
+    v = [1/i for i in range(1,n+1)]
+    r = [i+10 for i in range(1,n+1)]
     
-    print(getOptimalAssortment(n,v,r,5,log = False))
     
     
     
---
+++ PAO-TS.py
@@ -0,0 +1,90 @@
+
+import numpy as np
+import Optimal_Assortment
+
+#feasure dimension
+D = 1
+#item set
+N = []
+#cardinality constraint
+B = 6
+
+#2d-array
+Theta_g=[[]]
+
+#history trajectory
+H_TS=[]
+
+def Receive_x():
+    x = 1
+    return x
+
+def Generate_Theta(Nx):
+    theta = 1
+    return theta
+
+def Prod(x):
+    N_x = []
+    return N_x
+
+def getOptimalAssortment(Theta,Nx,x):
+    nx = len(Nx)
+    wx = [0]*n
+    rx = [0]*n
+    for i in range(n):
+        wx[i] = np.exp(np.dot(Theta_ts[Nx[i]:],x))
+        rx[i] = r[Nx[i]]
+    opt_as = Optimal_Assortment.getOptimalAssortment(n = nx, w = wx, r = rx, B=B, log = True)
+    for i in range(n):
+        opt_as[i] = Nx[opt_as[i]]
+        
+    return opt_as
+
+def getProbability(ast,x):
+    n = len(ast)
+    wx = np.array([0]*n)
+    for i in range(n):
+        wx[i] = np.exp(np.dot(Theta_g[ast[i]:],x))
+    
+    sum = np.sum(wx)+1
+    
+    prob = [wx[i]/sum for i in range(n)]
+    return prob
+
+def getOptimalValue(ast,x):
+    prob = getProbability(ast, x)
+    sum = 0
+    for i in range(len(ast)):
+        sum+=r[ast[i]]*prob[i]
+    return sum
+        
+def getCustomerPick(ast,x):
+    prob = getProbability(ast, x)
+    draw = np.random.choice(ast,1,p=prob)
+    return draw[0]
+
+
+
+def PAO_TS(T,r):
+    global H_TS
+    
+    for t in range(1,T+1):
+        x = Receive_x()
+        Nx = Prod(x)
+        Theta_ts = Generate_theta(Nx)
+        
+        opt_as_ts = getOptimalAssortment(Theta_ts, Nx, x)
+        opt_as_ora = getOptimalAssortment(Theta_g, Nx, x)
+        
+        getOptimalValue(opt_as_ora, x)
+        
+        I_t = getCustomerPick(opt_as_ts,x)
+        
+        H_TS.append([x,opt_as_ts,I_t])
+        
+if __name__=='__main__':
+    print('test')
+    
+    
+    
+        
