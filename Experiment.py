ba10286e47fc561ab10ec0742b8244ec97d28981
FireworksZ <zhaohuangjie518@gmail.com>
2021-05-01 18:37:12 +0800

    change file name, fix bug

--- Experiment.py
+++ Experiment.py
@@ -1,19 +1,42 @@
-from PAO-TS import PAO_TS
-from PAO-TS import PAO_TS_P
+import PAO_TS as PT 
+import PAO_TS as PTP 
+import numpy as np 
 
-import json
+
 
 #feature dimension
-D = 2
-L = 3
+D = 5
+K = 6
 #item set
-N = [1,2,3,4,5,6,7,8]
+N = [i+1 for i in range(20)]
 #cardinality constraint
-B = 5
+B = 20
 
-r = [8,7,6,5,4,3,2,1]
+r = np.random.uniform(size = len(N))
 #2d-array
-Theta_g=(2*np.random.normal(0,1.0,size=(D,L))-np.random.uniform(size = (D,L)))/np.sqrt(D)
-b_g = np.random.normal(0,1.0,size = len(N))
+Theta_p_g=(2*np.random.normal(0,1.0,size=(D,K))-np.random.uniform(size = (D,K)))/np.sqrt(D)
 
-prod_f = np.random.normal(0,1.0,size = (len(N),L))
+prod_f = np.random.normal(0,1.0,size = (len(N),K))
+
+Theta_np_g=np.matmul(prod_f,np.transpose(Theta_p_g)) 
+
+def Receive_x():
+    return np.random.rand(D)
+
+
+print(np.shape(Theta_np_g))
+print(np.shape(PT.Theta_g))
+
+T = 5
+
+customer_list = [Receive_x() for i in range(T)]
+
+re1,reo1 = PT.PAO_TS(T,r,customer_list)
+re2,reo2 = PT.PAO_TS_P(T,r,customer_list)
+
+res = {"reward1":re1,"reward_ora1":reo1,"reward2":re2,"reward_ora2":reo2}
+
+with open("test.json", 'w') as f:
+    json.dump(res, f)
+
+
--- sample.py
+++ sample.py
@@ -43,8 +43,8 @@ def Generate_theta(x,s,I,N):
 
     return trace1["theta"][-1]
 
-def Generate_theta_p(x,s,I,N,L,prod_f):
-    print(x,s,I,N)
+def Generate_theta_p(x,s,I,N,K,prod_f):
+    print(x,s,I,N,len(x))
     T = len(x)
     D = len(x[0])
     s = [one_hot(ss,N) for ss in s]
@@ -55,13 +55,12 @@ def Generate_theta_p(x,s,I,N,L,prod_f):
     model = pm.Model()
     with model:
         # Priors for unknown model parameters
-        theta = pm.Normal("theta",mu=0,sigma=1,shape=(D,L))
-        b = pm.Normal("b",mu = 0,sigma = 1,shape = (N))
+        theta = pm.Normal("theta",mu=0,sigma=1,shape=(D,K))
         p_list = []
         for t in range(T):
             #print(prod_f)
             #print(np.transpose(theta))
-            wt =  dot(dot(prod_f,transpose(theta)),x[t])+b
+            wt =  dot(dot(prod_f,transpose(theta)),x[t])
             swt = s[t]*wt
             sum_sw = sum(swt)
             p = exp(swt)/(1+sum_sw)
@@ -74,7 +73,7 @@ def Generate_theta_p(x,s,I,N,L,prod_f):
         step = pm.Metropolis()
         trace1 = pm.sample(20,n_init=100,step=step)
 
-    return trace1["theta"][-1],trace1["b"][-1]
+    return trace1["theta"][-1]
 '''
 D=2
 N=3
